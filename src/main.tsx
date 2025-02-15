a049bf92260c41e0c79c4063d4c71a51f9a1b828
JasrajChouhan <jasrajc945@gmail.com>
2025-02-15 14:18:40 +0530

    feat (provider): add React Query provider for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import { ReactQueryProvider } from './provider/react-query.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
   </StrictMode>,
 );
