5f34b7f3f68f0d5af2e56249d4e41f959ea76064
JasrajChouhan <jasrajc945@gmail.com>
2025-02-16 17:13:29 +0530

    feat(workspace): enhance useCreateWorkspace hook with mutation state management and improved toast import

--- src/hooks/workspace/useCreateWorkspace.ts
+++ src/hooks/workspace/useCreateWorkspace.ts
@@ -2,14 +2,14 @@ import { createWorkspaceRequest } from '@/api/workspace';
 import { useAuth } from '@/context';
 import { WorkspaceSchemaType } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useToast } from '../use-toast';
+import { useToast } from '@/hooks/use-toast';
 
 export const useCreateWorkspace = () => {
   const queryClient = useQueryClient();
   const { toast } = useToast();
   const { auth } = useAuth();
 
-  useMutation({
+  const { isError, isPending, isPaused, isSuccess, data, mutate, mutateAsync } = useMutation({
     mutationFn: (data: WorkspaceSchemaType) =>
       createWorkspaceRequest({
         token: auth?.token as string,
@@ -33,4 +33,14 @@ export const useCreateWorkspace = () => {
         queryClient.invalidateQueries({ queryKey: ['workspaces'] });
     },
   });
+
+  return {
+    isError,
+    isPending,
+    isPaused,
+    isSuccess,
+    data,
+    mutate,
+    mutateAsync,
+  };
 };
