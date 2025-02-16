a0eee0e7de245ea17e2143e10aaaf384458c4bf1
JasrajChouhan <jasrajc945@gmail.com>
2025-02-16 23:15:11 +0530

    fix(workspace): adjust layout structure in WorkspaceLayout for better child component rendering

--- src/components/layout/workspace.tsx
+++ src/components/layout/workspace.tsx
@@ -2,9 +2,11 @@ import WorkspaceSidebar from '../molecules/workspace-sidebar';
 
 export const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen overflow-hidden border border-slate-600">
-      <WorkspaceSidebar />
-      <div className="flex-1 overflow-auto p-4">{children}</div>
+    <div className="flex h-screen ">
+      <div className='h-full flex'>
+        <WorkspaceSidebar />
+        {children}
+      </div>
     </div>
   );
 };
