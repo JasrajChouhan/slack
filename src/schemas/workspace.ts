d7b94b682bbbe88fb5086db0e9c0252bc18e7990
JasrajChouhan <jasrajc945@gmail.com>
2025-02-16 18:51:45 +0530

    fix(workspace): make description field optional in WorkspaceSchema

--- src/schemas/workspace.ts
+++ src/schemas/workspace.ts
@@ -4,7 +4,5 @@ export const WorkspaceSchema = z.object({
   name: z.string().min(3, { message: 'Name must be at least 3 characters long' }).max(20, {
     message: 'Name must be at most 20 characters long',
   }),
-  descreption: z.string().min(3, { message: 'Description must be at least 3 characters long' }).max(100, {
-    message: 'Description must be at most 100 characters long',
-  }),
+  descreption: z.string().optional(),
 });
