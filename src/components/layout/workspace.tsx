b0bf274db59676d642fb40e053314b62344bd48f
JasrajChouhan <jasrajc945@gmail.com>
2025-02-19 15:02:12 +0530

    chore(format) : code format

--- src/components/layout/workspace.tsx
+++ src/components/layout/workspace.tsx
@@ -2,8 +2,8 @@ import WorkspaceSidebar from '../molecules/workspace-sidebar';
 
 export const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen ">
-      <div className='h-full flex'>
+    <div className="flex h-screen">
+      <div className="flex h-full">
         <WorkspaceSidebar />
         {children}
       </div>
