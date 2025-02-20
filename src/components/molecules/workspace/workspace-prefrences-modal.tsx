f0b770c202f446c25aa7b7d8d1bc609de28d7997
JasrajChouhan <jasrajc945@gmail.com>
2025-02-20 10:40:10 +0530

    feat(workspace): add delete workspace functionality to preferences modal

--- src/components/molecules/workspace/workspace-prefrences-modal.tsx
+++ src/components/molecules/workspace/workspace-prefrences-modal.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { useWorkspacePrefrence } from '@/context';
+import { Trash2Icon } from 'lucide-react';
 
 const WorkspacePrefrencesModal = () => {
   const { openCreateWorkspacePrefModal, setOpenCreateWorkspacePrefModal } = useWorkspacePrefrence();
@@ -13,7 +14,18 @@ const WorkspacePrefrencesModal = () => {
         <DialogHeader>
           <DialogTitle>Workspace Prefrences</DialogTitle>
         </DialogHeader>
-        hello
+
+        <div className="flex cursor-pointer flex-col gap-y-4 bg-white px-4 py-2 hover:bg-gray-50">
+          <div className="flex h-10 items-center justify-between rounded-lg border px-2">
+            <p className="text-sm font-semibold">Workspace Name</p>
+            <p className="text-sm font-semibold hover:underline">Edit</p>
+          </div>
+
+          <button className="flex cursor-pointer items-center gap-x-2 rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
+            <Trash2Icon className="size-5" />
+            <p className="text-sm font-semibold">Delete Workspace</p>
+          </button>
+        </div>
       </DialogContent>
       <DialogFooter></DialogFooter>
     </Dialog>
